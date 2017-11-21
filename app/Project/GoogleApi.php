2893d376e8968790f2b6eaff780020519c03d95d
Tyler McCall <tyler@flaremaster.net>
2017-11-21 17:21:48 -0500

    "finish" google calendar support
    Will work on cleaning it up later

--- app/Project/GoogleApi.php
+++ app/Project/GoogleApi.php
@@ -1,14 +1,78 @@
 <?php
 
 namespace App\Project;
+use Illuminate\Support\Facades\DB;
 
 /*
  * This class should contain functions for interacting with the google api for calendars
  * You likely need a login function that passes the users' credentials to the api
  * */
+
+ //TODO: Fix for if somebody doesn't have google calendar??
 class GoogleApi
 {
-    public function __construct(){
+    private $refreshToken;
+    private $accessToken;
+    private $googleClient;
+    private $calendarService;
+    private $uid;
 
+    private function refresh_token() {
+        //check if not expired
+        if ($this->$googleClient->isAccessTokenExpired()) {
+            $this->$googleClient->refreshToken($this->$refreshToken);
+            $this->$accessToken = $this->$googleClient->getAccessToken();
+            DB::update('UPDATE users SET access_token = ? WHERE id = ?',
+             [base64_encode(serialize($this->$accessToken)), $this->uid]);
+        }
     }
-}
+
+    public function __construct($uid){
+        //fetch the token from the user
+        $user = DB::select('SELECT * FROM users where id = ?', [$uid]);
+
+        if (!empty($users[0]->access_token)) {
+            $this->uid = $uid;
+            $this->$accessToken = unserialize(base64_decode($users[0]->access_token));
+            $this->$refreshToken = $users[0]->refresh_token;
+            $this->$googleClient = new Google_Client();
+            $this->$googleClient->setAccessType('offline');
+            $this->$googleClient->setAuthConfig('../../client_secrets.json');
+            $this->$googleClient->addScope(Google_Service_Calendar::CALENDAR);
+            $this->$googleClient->setAccessToken($this->$accessToken);
+            $this->refresh_token();
+            $this->$calendarService = new Google_Service_Calendar($this->$googleClient);
+        } else {
+            $this->$uid = -1;
+        }
+    }
+
+    private function get_current_week() {
+        $dto = new DateTime();
+        $year = $dto->format("Y");
+        $week = $dto->format("W");
+        $dto->setTime(0,0,0);
+        $result['start'] = $dto->setISODate($year, $week, 0)->format('Y-m-d\TH:i:sP');
+        $dto->setTime(23,59,59);
+        $result['end'] = $dto->setISODate($year, $week, 6)->format('Y-m-d\TH:i:sP');
+    }
+
+    public function fetch_events() {
+        //no user (return NULL)
+        if ($uid = -1) {
+            return NULL;
+        }
+        //refresh the current token
+        $this->refresh_token();
+        //get the start and end of the week (sunday to saturday)
+        $week = $this->get_current_week();
+        //set request params
+        $params = [
+            'singleEvents' => true,
+            'timeMax' => $week['end'],
+            'timeMin' => $week['start'],
+        ];
+        //fetch these events
+        $events = $this->$calendarService->events->listEvents('primary', $params);
+    }
+} 
--- routes/web.php
+++ routes/web.php
@@ -30,12 +30,46 @@ Route::get('/gcal/auth', function(){
     $client->setAuthConfig('../client_secrets.json');
     $client->setAccessType("offline");        // offline access
     $client->setIncludeGrantedScopes(true);   // incremental auth
-    $client->addScope(Google_Service_Calendar::CALENDAR_READONLY);
+    $client->addScope(Google_Service_Calendar::CALENDAR);
     $client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/gcal/authcallback');
     $auth_url = $client->createAuthUrl();
     header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
 });
 
 Route::get('/gcal/authcallback', function(){
-    print_r ($_GET);
+    
+    $auth = $_GET['code'];
+    $client = new Google_Client();
+    $client->setAuthConfig('../client_secrets.json');
+    $token = $client->fetchAccessTokenWithAuthCode($auth);
+    $calendarService = new Google_Service_Calendar($client);
+    /*
+    $params = [
+        'singleEvents'=>true,
+        'timeMax' => "2017-11-25T23:59:59Z",
+        'timeMin' => "2017-11-19T00:00:00Z",
+    ];
+
+    $events = $calendarService->events->listEvents('primary', $params);
+    
+
+    $googleTimes = [];
+    $googleEvent = [];
+    //whatever format wanted by the team
+    foreach ($events["items"] as $item) {
+        $startTime = $item->start->dateTime;
+        $endTime = $item->end->dateTime;
+        $googleEvent["start"] = $startTime;
+        $googleEvent["end"] = $endTime;
+        array_push($googleTimes, $googleEvent);
+    }
+    print_r($googleTimes);
+    */
+
+    DB::update('UPDATE users SET refresh_token = ?, access_token = ? WHERE id = ?',
+     [$token['refresh_token'], base64_encode(serialize($token)), Auth::id()]);
+    echo  "<script type='text/javascript'>";
+    echo "window.close();";
+    echo "</script>";
+    
 });
