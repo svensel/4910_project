2e257c302ba9f95557f6561a1a5fa903f3919c6b
Ubuntu <ubuntu@ip-172-31-88-223.ec2.internal>
2017-11-27 18:34:55 +0000

    fixed some css; still needs connection to other pages and database logic

--- resources/views/pages/groups.blade.php
+++ resources/views/pages/groups.blade.php
@@ -14,37 +14,42 @@
 
 	</h1>
     </ol>
-	<style>
-	.card {
-		box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-		transition: 0.3s;
-		border-raduis: 5px;
-	}
-	.container{
-		border-raduis: 5px 5px 0 0;
-	}
-	</style>
 	<?php
 		use App\Project\ModelFinder;
 		$groups=ModelFinder::getAuthUserGroups();
 		$num_groups=count($groups);	
 		$no_groups = false;
 		$ret='hell world';
-		echo "<div class='container'>";
+		echo "<div class='row'>
+			<div class='col-12'>";
 		if($num_groups>0){
 			//student belongs 
 			$no_groups = true;
 			for($i =0; $i < $num_groups; $i++){
-				echo "<div class='card'>.$ret.</div>";
+				echo "<div class='card text-white bg-primary o-hidden -100'>
+					<div class='card-body'>
+					</div>
+				.$ret.</div>";
 			}
 		} else {
 			$temp = 'no groups';
-			echo "<div class='card'>
-				<div class='container'>
-					<h4><b>$temp</b></h4>
-				</div>
-			</div>";
+				echo "	<div class='col-xl-6 col-sm-6 mb-3'>
+					<div class='card text-white bg-primary o-hidden -100'>
+					<div class='card-body'>
+						<div class='card-body-icon'>
+							<i class='fa-fw fa-group'>
+								$temp
+							</i>
+						</div>
+						<div class='mr-5'>
+						</div>
+						<a class='card-footer text-white clearfix small z-1' href='#'>
+						<span class='float-left'></span>
+						<span class='float-right'><i class='fa fa-angle-right'></i></span>
+						</a>
+					</div></div>
+				</div>";
 		}	
-		echo "</div>";
+		echo "</div></div>";
 	?>
 @endsection
