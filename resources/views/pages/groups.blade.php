28260a1325ce00c01decb3bf8184234de7e9cd02
svensel <svensel@g.clemson.edu>
2017-11-28 12:09:42 -0500

    Created Groups page

--- resources/views/pages/groups.blade.php
+++ resources/views/pages/groups.blade.php
@@ -1,11 +1,6 @@
 @extends('layouts.general')
 
-<!-- groups table has group_id as table key with person_id and class_id -->
-<!-- click group button shows groups and a button to generate schedule for a particular group-->
-
 @section('content')
-    <!-- modelfinder class app>>>project to pull data from database-->
-
     <ol class="breadcrumb">
         <li class="breadcrumb-item">
             <a href="">Groups</a>
@@ -15,27 +10,31 @@
 
     <?php
     use App\Project\ModelFinder;
-    //need to get group info from database
     $groups=ModelFinder::getAuthUserGroups();
     $num_groups=count($groups);
-    $temp = 'no groups';
-    $no_groups = false;
-    $ret='hell world';
     ?>
+
     <div class='row'>
         <div class='col-12'>
             <div class="row">
                 @if($num_groups>0)
-                    @php($no_groups = true)
                     @for($i=0; $i < $num_groups; $i++)
+                        <form id="{{$groups[$i]->id}}" hidden="hidden" method="POST" action="{{route('scheduleFinder')}}">
+                            {{csrf_field()}}
+                            <input title="" type="number" hidden="hidden" name="groupId" value="{{$groups[$i]->id}}">
+                        </form>
                         <div class='col-xl-6 col-sm-6 mb-3'>
-                            <div class='card text-white bg-primary o-hidden -100'>
+                            <div class='card text-white bg-{{$groups[$i]->course()->get()[0]->bg_color}} o-hidden -100'>
                                 <div class='card-body'>
-                                    <div class='card-body-icon'><i class='fa-fw fa-group'></i></div>
-                                    <div class='mr-5'>Name: {{$temp}}</div>
-
+                                    <div class='card-body-icon'><i class='fa fa-fw fa-group'></i></div>
+                                    <div class='mr-5'>Group Name: {{$groups[$i]->name}}</div><hr>
+                                    <div class="mr-5">Class Name: {{$groups[$i]->course->get()[0]->class_name}}</div><hr>
+                                    <div class="mr-5">Members:</div><br>
+                                    @foreach($groups[$i]->users()->get() as $user)
+                                        <div class="mr-5">{{$user->name}}</div>
+                                    @endforeach
                                 </div>
-                                <a class='card-footer text-white clearfix small z-1' >
+                                <a class='card-footer text-white clearfix small z-1' href="javascript:{}" onclick="document.getElementById({{$groups[$i]->id}}).submit()">
                                     <span class='float-left'>Generate schedule?</span>
                                     <span class='float-right'><i class='fa fa-angle-right'></i></span>
                                 </a>
@@ -43,20 +42,7 @@
                         </div>
                     @endfor
                 @else
-                    <div class='col-xl-6 col-sm-6 mb-3'>
-                        <div class='card text-white bg-primary o-hidden -100'>
-                            <div class='card-body'>
-                                <div class='card-body-icon'>
-                                    <i class='fa-fw fa-group'></i>
-                                </div>
-                                <div class='mr-5'>{{$temp}}</div>
-                                <a class='card-footer text-white clearfix small z-1' href='#'>
-                                    <span class='float-left'>Generate schedule?</span>
-                                    <span class='float-right'><i class='fa fa-angle-right'></i></span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+                    <div class="col-lg-3"><strong>You don't have any groups right now!</strong></div>
                 @endif
             </div>
         </div>
